12e8707a40cb7d36ef54d9102d8e0e8c226627fa
teodor-daniel1234 <115356255+teodor-daniel1234@users.noreply.github.com>
2023-11-26 17:11:14 +0200

    Comments + blink + multipleMaps
    Added comments and the blink function

--- Homework/MatrixGame/MatrixGame.ino
+++ Homework/MatrixGame/MatrixGame.ino
@@ -1,143 +1,158 @@
+#include "LedControl.h"
+#include <EEPROM.h>
 
-#include "LedControl.h" // Include LedControl library for controlling the LED matrix
 const int dinPin = 12;
 const int clockPin = 11;
 const int loadPin = 10;
-
+//controller
 const int xPin = A0;
 const int yPin = A1;
-// Create an LedControl object to manage the LED matrix
-LedControl lc = LedControl(dinPin, clockPin, loadPin, 1); // DIN, CLK, LOAD, No. DRIVER
-// Variable to set the brightness level of the matrix
+const int pinSW = A2;
+
+
+LedControl lc = LedControl(dinPin, clockPin, loadPin, 1);
+int selectedMap = 0;
 byte matrixBrightness = 2;
-// Variables to track the current and previous positions of the joystick-controlled LED
+
+const int numMaps = 3;  // Number of matrix maps
+byte matrixMap[numMaps][8][8] = {
+  {
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {1, 0, 0, 1, 0, 1, 0, 0},
+    {0, 1, 0, 0, 0, 1, 0, 0},
+    {0, 1, 0, 1, 0, 1, 0, 0},
+    {0, 1, 0, 0, 0, 1, 0, 0},
+    {0, 1, 0, 1, 0, 1, 0, 0},
+    {0, 0, 0, 0, 0, 0, 1, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0}
+  },
+  {
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 1, 0, 1, 0, 1, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 1, 0, 1, 0, 1, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 1, 0, 1, 0, 1, 0, 0},
+    {0, 0, 0, 0, 0, 0, 1, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0}
+  },
+  {
+    {0, 1, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 1, 0, 1, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 1, 0, 1, 0, 0},
+    {0, 1, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 1, 0, 1, 0, 0},
+    {0, 1, 0, 0, 0, 0, 1, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0}
+  }
+};
+
+byte matrixSize = 8;
 byte xPos = 0;
 byte yPos = 0;
 byte xLastPos = 0;
 byte yLastPos = 0;
-// Thresholds for detecting joystick movement
+
 const int minThreshold = 200;
 const int maxThreshold = 600;
 
-const byte moveInterval = 100; // Timing variable to control the speed of LED movement
-unsigned long long lastMoved = 0; // Tracks the last time the LED moved
+const byte moveInterval = 100;
+unsigned long long lastMoved = 0;
+bool matrixChanged = true;
 
-const byte matrixSize = 8 ;// Size of the LED matrix
-bool matrixChanged = true; // Flag to track if the matrix display needs updating
-// 2D array representing the state of each LED (on/off) in the matrix
-byte matrix[matrixSize][matrixSize] = {
-  {0, 0, 0, 0, 0, 0, 0, 0},
-  {0, 0, 0, 0, 0, 0, 0, 0},
-  {0, 0, 0, 0, 0, 0, 0, 0},
-  {0, 0, 0, 0, 0, 0, 0, 0},
-  {0, 0, 0, 0, 0, 0, 0, 0},
-  {0, 0, 0, 0, 0, 0, 0, 0},
-  {0, 0, 0, 0, 0, 0, 0, 0},
-  {0, 0, 0, 0, 0, 0, 0, 0}  
-};
-// Array representing each row of the LED matrix as a byte
-byte matrixByte[matrixSize] = {
-  B00000000,
-  B01000100,
-  B00101000,
-B00010000,
-  B00010000,
-  B00010000,
-  B00000000,
-  B00000000
-};
-byte matrixMap[matrixSize][matrixSize] = {
-  {0, 0, 0, 0, 0, 0, 0, 0},
-  {1, 0, 0, 1, 0, 1, 0, 0},
-  {0, 1, 0, 0, 0, 1, 0, 0},
-  {0, 1, 0, 1, 0, 1, 0, 0},
-  {0, 1, 0, 0, 0, 1, 0, 0},
-  {0, 1, 0, 1, 0, 1, 0, 0},
-  {0, 0, 0, 0, 0, 0, 1, 0},
-  {0, 0, 0, 0, 0, 0, 0, 0}  
-};
 void setup() {
   Serial.begin(9600);
-  // the zero refers to the MAX7219 number, it is zero for 1 chip
-  lc.shutdown(0, false); // turn off power saving, enables display
-  lc.setIntensity(0, matrixBrightness); // sets brightness (0~15 possible values)
-  lc.clearDisplay(0); // Clear the matrix display
-  matrix[xPos][yPos] = 1; // Initialize the starting position of the LED
-  //also here initialize the map of the matrix : the map of the matrix is here
-    // Initialize the starting position of the LED
-  matrix[xPos][yPos] = 1;
+  lc.shutdown(0, false);
+  lc.setIntensity(0, matrixBrightness);
+  lc.clearDisplay(0);
+  pinMode(pinSW, INPUT_PULLUP);
+  // Read the selected map from EEPROM and increment by 1
+  selectedMap = EEPROM.read(0);
+  selectedMap = (selectedMap + 1) % numMaps;
+  EEPROM.write(0, selectedMap);  // Save the updated selected map to EEPROM
+
+  Serial.println(selectedMap);
+
+  // Initialize the starting position of the LED
+  matrixMap[selectedMap][xPos][yPos] = 1;
 
   // Update the LED matrix display
   updateMatrix();
 }
+
 void loop() {
-  // Check if it's time to update the LED position
   if (millis() - lastMoved >= moveInterval) {
-    // Update the LED position
     updatePositions();
-    
-    // Reset the timer
     lastMoved = millis();
   }
+  Serial.print("selectedMap = ");
+  Serial.println(selectedMap);
 
-  // Check if the matrix display needs to be updated based on LED position change
   if (matrixChanged) {
     updateMatrix();
-    matrixChanged = false; // Reset the flag
+    matrixChanged = false;
   }
-}
 
-
-void updateByteMatrix() {
-  for (int row = 0; row < matrixSize; row++) {
-    lc.setRow(0, row, matrixByte[row]);  // set each ROW (or COL) at the same time
-  }
+  // Call blink function for the current position
+  blink(xPos, yPos);
 }
 
 void updateMatrix() {
   for (int row = 0; row < matrixSize; row++) {
     for (int col = 0; col < matrixSize; col++) {
-      // Use logical OR (||) to combine the character's position and the fixed map
-      lc.setLed(0, row, col, matrix[row][col] || matrixMap[row][col]);
+      lc.setLed(0, row, col, matrixMap[selectedMap][row][col]);
     }
   }
 }
 
 void updatePositions() {
-  // Read analog values from the joystick (X and Y axes)
   int xValue = analogRead(xPin);
   int yValue = analogRead(yPin);
-
-  // Calculate the potential new positions
+  int hasMoved = false;
   byte newXPos = xPos;
   byte newYPos = yPos;
 
-  if (xValue < minThreshold && newXPos > 0) {
+  if (xValue < minThreshold && newXPos > 0 && hasMoved == false) {
     newXPos--;
-  } else if (xValue > maxThreshold && newXPos < matrixSize - 1) {
+    hasMoved = true;
+  } else if (xValue > maxThreshold && newXPos < matrixSize - 1 && hasMoved == false) {
     newXPos++;
+        hasMoved = true;
+
   }
 
-  if (yValue < minThreshold && newYPos < matrixSize - 1) {
-    newYPos++;
-  } else if (yValue > maxThreshold && newYPos > 0) {
-    newYPos--;
+  if (yValue < minThreshold && newYPos < matrixSize - 1 && hasMoved == false) {
+    newYPos++;    hasMoved = true;
+
+  } else if (yValue > maxThreshold && newYPos > 0 && hasMoved == false) {
+    newYPos--;    hasMoved = true;
+
   }
 
-  // Check if the target position is clear
-  if (matrix[newXPos][newYPos] == 0 && matrixMap[newXPos][newYPos] == 0) {
-    // Update the matrixChanged flag and the matrix array
+  if (matrixMap[selectedMap][newXPos][newYPos] == 0) {
     matrixChanged = true;
-    matrix[xPos][yPos] = 0; // Clear the current position
-    matrix[newXPos][newYPos] = 1; // Set the new position
-
-    // Update the last positions
+    matrixMap[selectedMap][xPos][yPos] = 0;
+    matrixMap[selectedMap][newXPos][newYPos] = 1;
     xLastPos = xPos;
     yLastPos = yPos;
-
-    // Update the current positions
     xPos = newXPos;
     yPos = newYPos;
+  }
+}
+
+void blink(byte x, byte y) {
+  static unsigned long lastBlinkTime = 0;
+  static bool isOn = true;
+  const unsigned long blinkInterval = 400; // Adjust the blink interval as needed
+
+  if (millis() - lastBlinkTime >= blinkInterval) {
+    // Toggle the blink state
+    isOn = !isOn;
+    lc.setLed(0, x, y, isOn);
+    
+    // Update the last blink time
+    lastBlinkTime = millis();
   }
 }
 
